3c72277a692710676f2a3f2bda175fff6db15b0e
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-01-18 09:10:57 +0100

    ccc

--- Lezione6-Ripassone/js/script3.js
+++ Lezione6-Ripassone/js/script3.js
@@ -1,6 +1,5 @@
 let numE = document.getElementById("numE");
 function estrai() {
-
   let numeri = [];
 
   for (let i = 0; i < 90; i++) {
@@ -17,72 +16,65 @@ function estrai() {
     // } else {
     //   i--;
     // }
-    
-    console.log(`Estraggo il ${i+1}^ numero...`);
+
+    console.log(`Estraggo il ${i + 1}^ numero...`);
     estraiNumero(numeri, numEstratti);
   }
-  
-  
+
   let string = "";
   for (let i = 0; i < numEstratti.length; i++) {
     string += `${i + 1}) <b>${numEstratti[i]}</b> &nbsp;&nbsp;`;
   }
-  numE.innerHTML = "<p>"+string+"</p>";
-
+  numE.innerHTML = "<p>" + string + "</p>";
 }
 
 function estraiNumero(numeri, numEstratti) {
-    let numEstratto = Math.ceil(Math.random() * 90);
-    if (numeri.includes(numEstratto)) {
-      numeri.splice(numeri.indexOf(numEstratto), 1);
-      numEstratti.push(numEstratto);
-    } else {
-      i--;
-    }
+  let numEstratto = Math.ceil(Math.random() * 90);
+  if (numeri.includes(numEstratto)) {
+    numeri.splice(numeri.indexOf(numEstratto), 1);
+    numEstratti.push(numEstratto);
+  } else {
+    i--;
+  }
 }
 
 
 function estraiProf() {
+  let numDaEstrarre = document.getElementById("numDaEstrarre").value;
 
-    let numDaEstrarre = document.getElementById("numDaEstrarre").value;
+  if (numDaEstrarre <= 90) {
+    let numEstratti = [];
+    let giri = 0;
 
-    if (numDaEstrarre <= 90) {
-        
-        
-        
-        
-        let numEstratti = [];
-        let giri = 0;
-        
-        // for (let i = 0; i < numDaEstrarre; i++) {
-        //     let numero = Math.ceil(Math.random()*90);
-            
-        //     if(numEstratti.includes(numero)){
-        //         i--;
-        //     }else{
-        //         numEstratti.push(numero);
-        //     }
-        //     giri++;
-        // }
+    // for (let i = 0; i < numDaEstrarre; i++) {
+    //     let numero = Math.ceil(Math.random()*90);
 
-    while(numEstratti.length<5){
-            let numero = Math.ceil(Math.random()*90);
-        
-            if (numEstratti.includes(numero)) {
-                    numEstratti.push(numero);
-                }
-            
-            
-            console.log(numEstratti);
-            console.log(giri);
-        else{
-            alert("Amico hai messo più numeri di quanti ce ne siano nel gioco del lotto")
-        }
+    //     if(numEstratti.includes(numero)){
+    //         i--;
+    //     }else{
+    //         numEstratti.push(numero);
+    //     }
+    //     giri++;
+    // }
 
-        numEstratti.forEach(numero => {
-            numE.innerHTML += "<li>"+numero+"</li>"
-        });
+    while (numEstratti.length < 5) {
+      let numero = Math.ceil(Math.random() * 90);
+
+      if (numEstratti.includes(numero)) {
+        numEstratti.push(numero);
+      }
+
+      console.log(numEstratti);
+      console.log(giri);
     }
+  } else {
+    alert(
+      "Amico hai messo più numeri di quanti ce ne siano nel gioco del lotto"
+    );
+  }
+
+  numEstratti.forEach((numero) => {
+    numE.innerHTML += "<li>" + numero + "</li>";
+  });
 }
 
-        
