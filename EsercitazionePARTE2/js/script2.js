063bed2f7a924a4636e63fe4186d26b038114065
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-01-27 15:29:58 +0100

    Fine esercizi

--- EsercitazionePARTE2/js/script2.js
+++ EsercitazionePARTE2/js/script2.js
@@ -44,21 +44,70 @@ function ruota() {
     console.log(stringa);
     cont++;
     if (cont == stringa.length) {
-        clearInterval(intervallo);
+      clearInterval(intervallo);
     }
   }, 1000);
 }
 
 function startDash() {
-    let numero = window.prompt("Inserisci un numero lungo");
-    let arrayNumeri = numero.slice('');
-    let stringa = "";
-    for (let i = 0; i < arrayNumeri.length; i++) {
-        if (arrayNumeri[i]%2 == 0 && arrayNumeri[i+1]%2 == 0) {
-            stringa += arrayNumeri[i] + "-";
-        }else{
-            stringa += arrayNumeri[i];
-        }
+  let numero = window.prompt("Inserisci un numero lungo");
+  let arrayNumeri = numero.slice("");
+  let stringa = "";
+  for (let i = 0; i < arrayNumeri.length; i++) {
+    if (arrayNumeri[i] % 2 == 0 && arrayNumeri[i + 1] % 2 == 0) {
+      stringa += arrayNumeri[i] + "-";
+    } else {
+      stringa += arrayNumeri[i];
     }
-    console.log(stringa);
+  }
+  console.log(stringa);
+}
+
+let array = [];
+function startElementi() {
+  let numElementi = prompt("Quanti elementi voi inserire?");
+
+  for (let i = 0; i < Number(numElementi); i++) {
+    let elem = prompt("Inserisci l'elemento:");
+    array[i] = elem;
+  }
+  document.getElementById("newButton").innerHTML =
+    '<button type="button" onclick="stampaElementi(); return false">Stampa Elementi</button>';
+}
+
+function stampaElementi() {
+  array.forEach((element) => {
+    console.log(element);
+  });
+}
+
+function rimuoviDuplicati() {
+  let arrayNoDuplicati = [];
+  for (let i = 0; i < array.length; i++) {
+    let elem = array[i];
+    if (arrayNoDuplicati.indexOf(elem) === -1) {
+      arrayNoDuplicati.push(elem);
+    }
+  }
+  console.log(arrayNoDuplicati);
+}
+function sommaElementi() {
+  let array1 = [1, 0, 2, 4, 6];
+  let array2 = [0, 4, 5, 8, 7];
+  let arraysomma = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    arraysomma[i] = array1[i] + array2[i];
+  }
+  console.log(arraysomma);
+}
+
+function unisciArray() {
+  let array1 = [1, 0, 2, 4, 6];
+  let array2 = [0, 4, 5, 8, 7];
+  let arrayUnico = array1.concat(array2);
+
+  let noDuplicati = new Set(arrayUnico);
+  let arrayNoDuplicati = Array.from(noDuplicati);
+  console.log(arrayNoDuplicati);
 }
