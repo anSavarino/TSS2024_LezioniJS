3eec931a9b0ae2b1050969273b244bb4ccb96b8a
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-02-01 13:57:34 +0100

    Lezione 01/02/2024

---
+++ EsercitazioneColloquio/js/script.js
@@ -0,0 +1,56 @@
+// ordinare un array [2,1,18,22,0,42,8] trovare maggiore e trovare pari e dispari
+
+// Estrazione n numeri da 0 a 100
+
+// Estrazione n numeri da 25 a 50
+
+// *
+// **
+// ***
+
+
+let numeriEstratti = [];
+
+// let giri = 0;
+
+// for (let i = 0; i < 20; i++) {
+//     let numero = Math.ceil((Math.random()*25)+25);
+
+//     if(numeriEstratti.includes(numero)){
+//         i--;
+//     }else{
+//         numeriEstratti.push(numero);
+//     }
+//     giri++;
+// }
+
+// console.log(numeriEstratti.sort((a, b) => a - b));
+// console.log(giri);
+
+
+
+
+
+
+
+// while (numeriEstratti.length < 6) {
+//     let numero2 = Math.ceil((Math.random()*26)+24);
+//     if(!numeriEstratti.includes(numero2)){
+//         numeriEstratti.push(numero2);
+//     }
+// }
+// console.log(numeriEstratti.sort((a, b) => a - b));
+
+// let setPerEstrazione = [];
+
+// for (let i = 1; i <= 90; i++) {
+//     setPerEstrazione.push(i);
+// }
+
+// for (let i = 0; i < 6; i++) {
+//     let numero = setPerEstrazione[Math.floor(Math.random() * setPerEstrazione.length)];;
+//     numeriEstratti.push(numero);
+//     setPerEstrazione.splice(numero, 1);
+// }
+// console.log(numeriEstratti.sort((a, b) => a - b));
+
